332b7a41c146d4033f0e7ded2a33ffd454ed16af
leonardo.mendes@4linux.com.br <leonardo.mendes@4linux.com.br>
2019-08-10 10:03:15 -0300

    commit inicial

---
+++ app.py
@@ -0,0 +1,23 @@
+import json
+from flask import Flask, jsonify, render_template, request
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/save', methods = ['POST'])
+def save():
+	dados = request.get_json()
+	with open('cache.dat', 'w') as f:
+		f.write(json.dumps(dados))
+	return jsonify({'mensagem' : 'Dados Gravados'})
+
+
+@app.route('/status')
+def status():
+	return jsonify ({'mensagem':'Ola Flask!'})
+
+if __name__ == '__main__':
+	app.run(port =5000, debug=True, host='0.0.0.0')
